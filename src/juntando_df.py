d0c3c59a9ef12c8aa91d54f3d5a69d63014f7ea2
sirius1618 <sagittarius_a1.618@proton.me>
2025-09-11 11:25:52 -0300

    chore: Termino da Classe JuntarArquivoCsv   - Adicionado a funcionalidade de salvamento por parametro passado     - salvar um atributo especifico.     - salvar sem duplicata de sensores.     - salvar arquivo completo compilado.

--- src/juntando_df.py
+++ src/juntando_df.py
@@ -1,44 +1,61 @@
-# pegar dados csv 
-# juntar dados csv 
-# salvar dados (salvar sem duplicata, salvar completo, salvar uma parte)
-
 import glob
 import pandas as pd
 import os
 
-class Join: 
-    # TODO: colocar o tipo de arquivo que sera lido e salvo:
+class JuntarArquivoCsv:
+ 
     def __init__(self, caminho_dados:str):
         self.caminho_dados = caminho_dados
     
-    def join_csv(self) -> pd.DataFrame: 
-        # TODO: colocar um tratamento
-        joinDadosCsv = glob.glob(os.path.join(self.caminho_dados,"**/*.csv"),recursive=True)
+    def juntar_csv(self) -> pd.DataFrame: 
+        juntar_dados_csv = glob.glob(os.path.join(self.caminho_dados,"**/*.csv"),recursive=True)
 
-        # TODO: colocar tratamento aqui:
         lista_df = []
-        for arquivo in joinDadosCsv:
+        for arquivo in juntar_dados_csv:
             try:
                 df = pd.read_csv(arquivo, on_bad_lines="warn")
                 lista_df.append(df)
             except Exception as e:
                 print(f"Erro ao ler {arquivo} -> {e}")
         
-        return pd.concat(lista_df,ignore_index=True)
+        if lista_df: 
+            return pd.concat(lista_df,ignore_index=True)
+        else: 
+            raise ValueError("lista concatenada de dados CSV vazia")
 
-    def save_df_csv(self, sem_duplicata:bool = False, completo:bool = True, salvar_atributo:str = None):
+    def salvar_df_csv(self, sem_duplicata_sensor:bool = False, completo:bool = True, salvar_atributo:str = None):
 
-        dados_csv = self.join_csv(self.caminho_dados)
-        caminho_salvar = "data"
+        dados_csv = self.juntar_csv()
+        
+        caminho_salvar = {
+            "arquivo_completo" : "../data/compilado_dados.csv",
+            "arquivo_sem_duplicata_sensor" : "../data/compilado_dados_sem_duplicata_sensor.csv",
+            "arquivo_atributo_especifico" : "../data/compilado_atributo_especifico.csv",
 
-        if not os.path.exists(caminho_salvar):
-            raise FileNotFoundError(f"Diretorio não encontrado: {caminho_salvar}")
+            }
 
-        if self.completo:
+        diretorio = os.path.dirname(caminho_salvar["arquivo_completo"])
+        if not os.path.exists(diretorio):
+            raise FileNotFoundError(f"Diretorio não encontrado: {diretorio}")
+
+        if completo:
             try:
-                dados_csv.to_csv("../data/join_data_sensor.csv",index=False)
+                dados_csv.to_csv(caminho_salvar["arquivo_completo"],index=False)
             except Exception as e: 
-                print(f"")
+                print(f"Erro ao salvar o arquivo CSV: {e}")
+
+        if sem_duplicata_sensor:
+            try:
+                dados_csv_sem_duplicata_sensor = dados_csv.drop_duplicates(subset=["entity_id"])
+                dados_csv_sem_duplicata_sensor.to_csv(caminho_salvar["arquivo_sem_duplicata_sensor"], index=False)
+            except Exception as e:
+                print(f"Erro ao salvar o arquivo CSV: {e}")
+
+        if salvar_atributo != None:
+            if salvar_atributo in dados_csv.columns: 
+                dados_csv[salvar_atributo].to_csv(caminho_salvar["arquivo_atributo_especifico"], index=False)
+            else: 
+                print(f"Coluna do data frame atribuido não existe nesse data frame")
 
 
 
